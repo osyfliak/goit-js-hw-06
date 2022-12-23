e8002eec1e571443ed467b7dfe005e795872825b
Osyfliak Ivan <osuflak22@gmail.com>
2022-12-23 16:31:11 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,23 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+  buttonCreate: document.querySelector("[data-create]"),
+  buttonDestroy: document.querySelector("[data-destroy]"),
+  input: document.querySelector("input"),
+  div: document.querySelector("#boxes")
+}
+
+refs.buttonCreate.addEventListener('click', onCreateBox)
+refs.buttonDestroy.addEventListener('click', onDestoroyBox)
+
+function onCreateBox() {
+  
+}
+
+function onDestoroyBox() {
+  refs.input.value = '0';
+  // refs.div.amout(0);
 }
